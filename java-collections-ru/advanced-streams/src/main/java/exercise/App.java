fe88fb92572619825088c2597ac474a6885a93e8
@hexlet/cli <support@hexlet.io>
2023-10-06 23:56:26 +0200

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -7,23 +7,15 @@ import java.util.stream.Stream;
 // BEGIN
 public class App {
     public static String getForwardedVariables(String configData) {
-        List<String> environmentLines = Arrays.stream(configData.split("\n"))
+        return String.join("," ,Arrays.stream(configData.split("\n"))
                 .filter(line -> line.startsWith("environment="))
                 .map(line -> line.replace("environment=", ""))
                 .map(line -> line.replace("\"", ""))
-                .toList();
-
-        List<String> variables = environmentLines.stream()
                 .flatMap(line -> Stream.of(line.split(",")))
                 .filter(variable -> !variable.isBlank())
                 .filter(variable -> variable.startsWith("X_FORWARDED_"))
-                .toList();
-
-        List<String> result = variables.stream()
                 .map(variable -> variable.replace("X_FORWARDED_" , ""))
-                .toList();
-
-        return String.join(",", result);
+                .toList());
     }
 }
 //END
