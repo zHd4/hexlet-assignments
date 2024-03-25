0ea411208fa16753140896cf0e8b8902c153384b
@hexlet/cli <support@hexlet.io>
2024-03-25 21:05:07 +0100

    submit java-spring-ru/dependency-injection

--- java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
@@ -1,6 +1,7 @@
 package exercise.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,5 +20,48 @@ import exercise.repository.CommentRepository;
 import exercise.exception.ResourceNotFoundException;
 
 // BEGIN
+@RestController
+@RequestMapping("/comments")
+public class CommentsController {
+    @Autowired
+    private CommentRepository commentRepository;
 
+    @GetMapping
+    public ResponseEntity<List<Comment>> index() {
+        return ResponseEntity.ok(commentRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Comment> show(@PathVariable long id) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment with id " + id + " not found"));
+
+        return ResponseEntity.ok(comment);
+    }
+
+    @PostMapping
+    public ResponseEntity<Comment> create(@RequestBody Comment comment) {
+        commentRepository.save(comment);
+        return ResponseEntity.status(HttpStatus.CREATED).body(comment);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Comment> update(@PathVariable long id, @RequestBody Comment commentData) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment with id " + id + " not found"));
+
+        comment.setBody(commentData.getBody());
+
+        commentRepository.save(comment);
+        return ResponseEntity.ok(comment);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment with id " + id + " not found"));
+
+        commentRepository.delete(comment);
+    }
+}
 // END
