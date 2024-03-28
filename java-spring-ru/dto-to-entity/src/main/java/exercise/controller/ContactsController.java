17fc0ae941a81cdf999751331cc72599096d3a48
@hexlet/cli <support@hexlet.io>
2024-03-28 18:59:45 +0100

    submit java-spring-ru/dto-to-entity

--- java-spring-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
+++ java-spring-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
@@ -1,19 +1,16 @@
 package exercise.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
+import exercise.dto.ContactCreateDTO;
+import exercise.dto.ContactDTO;
 import exercise.model.Contact;
 import exercise.repository.ContactRepository;
-import exercise.dto.ContactDTO;
-import exercise.dto.ContactCreateDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contacts")
